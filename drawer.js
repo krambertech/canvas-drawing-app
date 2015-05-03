e715bbab533cb2f3840e1a20e71577c62d66a2df
Kateryna Porshnieva <k.porshnieva@gmail.com>
2015-05-03 08:04:10 +0300

    Made brash size and color changable

--- drawer.js
+++ drawer.js
@@ -5,6 +5,7 @@ canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var brushSize = 20;
+var brushColor = 20;
 var isMouseDown = false;
 
 ctx.lineWidth = brushSize;
@@ -19,19 +20,30 @@ var draw = function(e) {
 		ctx.beginPath();
 		ctx.moveTo(e.clientX, e.clientY);
 	}
-}
+};
 
 var mouseUp = function(e) {
 	isMouseDown = false;
 	ctx.closePath();
-}
+};
 
 var mouseDown = function(e) {
 	isMouseDown = true;
 	ctx.moveTo(e.clientX, e.clientY);
 	draw(e);
-}
+};
 
 canvas.addEventListener('mousedown', mouseDown);
 canvas.addEventListener('mouseup', mouseUp);
 canvas.addEventListener('mousemove', draw);
+
+var changeBrushColor =  function(value){
+    brushColor = value;
+    ctx.strokeStyle = brushColor;
+    ctx.fillStyle=brushColor;
+};
+
+var changeBrushSize =  function(value){
+    brushSize = +value;
+    ctx.lineWidth = brushSize;
+};
